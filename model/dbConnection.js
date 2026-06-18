b7f6993a9d0a0a0f1b44c2b62abfcce478303a16
semsem2005 <vbs24euu@uea.ac.uk>
2026-06-18 18:41:32 +0100

    Delete model directory

--- model/dbConnection.js
+++
@@ -1,7 +0,0 @@
-const postgres = require('postgres');
-const dotenv = require('dotenv').config();
-const sql = postgres(`${process.env.DB_TYPE}://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
-
-exports.getConnection = () => {
-    return sql;
-}
